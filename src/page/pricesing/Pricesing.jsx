d63aab5a33ea72d4b1bb746e939fcba75fc7b744
aitdevelopment <aftabgongit@gmail.com>
2024-10-17 12:29:53 +0600

    router-setup

---
+++ src/page/pricesing/Pricesing.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Pricesing = () => {
+  return (
+    <div>Pricesing</div>
+  )
+}
+
+export default Pricesing
---
+++ src/router/router.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from '../page/home/Home'
+import LandingPage from '../page/landingPage/LandingPage'
+import Services from '../page/services/Services'
+import Portfolio from '../page/portfolio/Portfolio'
+import Pricesing from '../page/pricesing/Pricesing'
+import Team from '../page/team/Team'
+import Technology from '../page/technology/Technology'
+import Contact from '../page/contact/Contact'
+import PageNotFound from '../page/pageNotFound/PageNotFound'
+import Layout from '../layout/Layout'
+
+const router = createBrowserRouter([
+   {
+    path:'/',
+    element:<Layout/>,
+    children:[
+        {
+            path:"/",
+            element:<LandingPage/>
+          },
+          {
+              path:"/home",
+              element:<Home/>
+          },{
+              path:"/services",
+              element:<Services/>
+          },{
+              path:"/portfolio",
+              element:<Portfolio/>
+          },{
+              path:"/pricesing",
+              element:<Pricesing/>
+          },{
+              path:"/our-team",
+              element:<Team/>
+          }
+          ,{
+              path:"/technology",
+              element:<Technology/>
+          }
+          ,{
+              path:"/contact-us",
+              element:<Contact/>
+          }
+          ,{
+              path:"*",
+              element:<PageNotFound/>
+          }
+    ]
+   }
+  ])
+
+export default router
